9543baa6a482ab8b8a5d600d44f1070ee5a9499b
ovciina <ovciina@gmail.com>
2022-05-05 13:39:30 +0200

    Updates

---
+++ precache-manifest.49118e09620afb3f0a588a79c622aaab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a77f7271c6dd0fd45cf3",
+    "url": "/shopping-app/static/css/main.7ecb37cc.chunk.css"
+  },
+  {
+    "revision": "a77f7271c6dd0fd45cf3",
+    "url": "/shopping-app/static/js/main.a77f7271.chunk.js"
+  },
+  {
+    "revision": "9f6f6cec55adb720d38c",
+    "url": "/shopping-app/static/js/1.9f6f6cec.chunk.js"
+  },
+  {
+    "revision": "4290961605600cf0232a",
+    "url": "/shopping-app/static/css/2.6f774b61.chunk.css"
+  },
+  {
+    "revision": "4290961605600cf0232a",
+    "url": "/shopping-app/static/js/2.42909616.chunk.js"
+  },
+  {
+    "revision": "162d6b313c2791aa259e",
+    "url": "/shopping-app/static/js/runtime~main.162d6b31.js"
+  },
+  {
+    "revision": "6093f6b9a79986546b89b5e8165af224",
+    "url": "/shopping-app/static/media/img_1.6093f6b9.png"
+  },
+  {
+    "revision": "0ca5017954bccadd4a18845d0ff525da",
+    "url": "/shopping-app/static/media/img_2.0ca50179.png"
+  },
+  {
+    "revision": "9be3af0482dfc988ace17ed2c4db2d53",
+    "url": "/shopping-app/static/media/img_3.9be3af04.png"
+  },
+  {
+    "revision": "b311f811ee2ef4027a71ec78d7d3c278",
+    "url": "/shopping-app/index.html"
+  }
+];
